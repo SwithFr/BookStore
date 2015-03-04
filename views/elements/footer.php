558d208420313221f41bf7aa4d122e4c06a135dd
Jeremy Smith <contact@swith.fr>
2015-03-04 17:43:14 +0100

    add footer

---
+++ views/elements/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+    <div class="footer__content">
+        <h2 class="footer__title">Book Store</h2>
+        <nav class="nav">
+            <div class="nav__content">
+                <ul class="nav__menu">
+                    <li class="nav__menu__item"><a href="./views/books/authorsList.html">Auteurs</a></li>
+                    <li class="nav__menu__item"><a href="./views/books/editorsList.html">Editeurs</a></li>
+                    <li class="nav__menu__item"><a href="./views/books/categoriesList.html">Genres</a></li>
+                    <li class="nav__menu__item"><a href="./views/books/languagesList.html">Langues</a></li>
+                    <li class="nav__menu__item"><a href="./views/books/yearsList.html">Années</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</footer>
+</body>
+</html>
