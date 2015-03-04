0ac9d5a819b5ddae1c046def951b634570dc5005
Jeremy Smith <contact@swith.fr>
2015-03-04 17:43:34 +0100

    add header

---
+++ views/elements/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?= D_ASSETS . DS; ?>css/main.css"/>
+    <title>BookStore</title>
+</head>
+<body>
+<header class="header header--home">
+    <div class="header__content">
+        <div class="header__title">
+            <p>Une nouvelle manière de découvir,</p>
+            <p>partager et lire des livres</p>
+        </div>
+        <div class="header__login"><a href="./views/users/register.html" class="btn btn--white">Créer un compte</a><a href="./views/users/login.html" class="btn btn--white">Connexion</a>
+        </div>
+    </div>
+</header>
