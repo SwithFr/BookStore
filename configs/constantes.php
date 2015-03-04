4d5b9a77fc662aec917d9df52b3d8b7632db8983
Jeremy Smith <contact@swith.fr>
2015-03-04 15:45:17 +0100

    ajout des constantes

--- configs/constantes.php
+++ configs/constantes.php
@@ -2,7 +2,13 @@
 
 # PATHS
 define('BASE',$_SERVER['SCRIPT_NAME']);
+define('D_ASSETS','./assets');
+define('D_COMPONENTS','./components');
+define('D_CONFIGS','./configs');
 define('D_CONTROLLERS','./controllers');
+define('D_HELPERS','./helpers');
+define('D_MODELS','./models');
+define('D_VIEWS','./views');
 
 # UTILES
 define('DS', DIRECTORY_SEPARATOR);
