896ccd08d5748ba85adc8acec101693f5589f42e
Jeremy Smith <contact@swith.fr>
2015-03-04 17:41:10 +0100

    add content

---
+++ views/books/index.php
@@ -0,0 +1,31 @@
+<div class="section vedettes">
+    <div class="section__header">
+        <h2 class="section__title">En vedette :</h2><a href="./views/books/byAuthor.html" class="section__readMore">Voir tout le classement</a>
+    </div>
+    <div class="section__block"><img src="./assets/img/book-img.jpg" class="section__block__img">
+        <h3 class="section__block__title">Un titre</h3>
+        <p class="section__block__author">Auteur</p>
+        <p class="section__block__content">Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus                   lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve.</p><a href="./views/books/single.html" class="section__readMore">En voir plus</a>
+    </div>
+    <div class="section__block"><img src="./assets/img/book-img.jpg" class="section__block__img">
+        <h3 class="section__block__title">Un titre</h3>
+        <p class="section__block__author">Auteur</p>
+        <p class="section__block__content">Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus                   lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve.</p><a href="./views/books/single.html" class="section__readMore">En voir plus</a>
+    </div>
+</div>
+<div class="section author">
+    <div class="section__header">
+        <h2 class="section__title"><a href="./views/books/byAuthor.html">Un auteur au hasard</a></h2><a href="./views/books/byAuthor.html" class="section__readMore">Voir plus du même auteur</a>
+    </div>
+    <div class="section__block">
+        <h3 class="section__block__title">Nom de l'auteur</h3>
+        <p class="section__block__year">1700 - 1800</p>
+        <p class="section__block__content">Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus                   lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve.</p>
+        <div class="author__count">
+            <p class="nb author__nbBooks">20</p>
+            <p class="nb author__eval">4.5</p>
+        </div>
+    </div>
+    <div class="section__block"><img src="./assets/img/vHugo.jpg" class="section__block__img"></div>
+</div>
+</div>
