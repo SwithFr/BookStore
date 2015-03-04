6d90a35d4529772fad7907ccc141cbe7090dff02
Jeremy Smith <contact@swith.fr>
2015-03-04 16:23:22 +0100

    add commentaires

--- components/DbProvider.php
+++ components/DbProvider.php
@@ -4,13 +4,29 @@
 class DbProvider 
 {
 
+    /**
+     * Les paramettres de la connexion à la base de données
+     * @var array|mixed
+     */
     private $settings = [];
+
+    /**
+     * La connexion PDO
+     * @var null|PDO
+     */
     private $db = null;
+
+    /**
+     * L'instance du singleton
+     * @var null|DbProvider
+     */
     private static $_instance = null;
 
     private function __construct()
     {
+        # On récupère le tableau d'options de database.php
         $this->settings = require(D_CONFIGS . DS . 'database.php');
+
         try {
             $this->db = new PDO(
                 'mysql:dbname='. $this->settings['dbName'] . ';host:' . $this->settings['host'],
@@ -25,6 +41,10 @@ class DbProvider
         }
     }
 
+    /**
+     * Permet de récupérer la connexion PDO
+     * @return null|PDO
+     */
     public static function getDb()
     {
         if (is_null(self::$_instance)) {
