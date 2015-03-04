418fe2b7340506b86be29bce5b951e40d1265d49
Jeremy Smith <contact@swith.fr>
2015-03-04 15:49:33 +0100

    update defautl layout

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -2,9 +2,13 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?= D_ASSETS . DS; ?>css/main.css"/>
     <title>BookStore</title>
 </head>
 <body>
+
+<?php include(D_VIEWS . DS . $controller->view); ?>
 
 </body>
 </html>
