6d9e10d484e1edc298ad7c7b27a9eb105d3958dd
Jeremy Smith <contact@swith.fr>
2015-03-04 16:14:50 +0100

    ajout de la connexion via le singleton DbProvider

--- models/AppModel.php
+++ models/AppModel.php
@@ -3,5 +3,14 @@
 
 class AppModel 
 {
+    protected  $db = null;
+    protected $table = null;
 
+    function __construct()
+    {
+        if (!is_null($this->db)) {
+           $this->db = DbProvider::getDb();
+        }
+        var_dump($this->db);die();
+    }
 } 
