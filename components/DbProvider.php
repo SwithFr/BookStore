466854116db8588adfa3b4d18d5d995093895399
Jeremy Smith <contact@swith.fr>
2015-03-04 16:13:09 +0100

    add DbProvider

---
+++ components/DbProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+
+class DbProvider 
+{
+
+    private $settings = [];
+    private $db = null;
+    private static $_instance = null;
+
+    private function __construct()
+    {
+        $this->settings = require(D_CONFIGS . DS . 'database.php');
+        try {
+            $this->db = new PDO(
+                'mysql:dbname='. $this->settings['dbName'] . ';host:' . $this->settings['host'],
+                $this->settings['username'],
+                $this->settings['password'],
+                $this->settings['options']
+            );
+            $this->db->query('SET CHARACTER SET UTF8');
+            $this->db->query('SET NAMES UTF8');
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+
+    public static function getDb()
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new App();
+        }
+        return self::$_instance->db;
+    }
+
+} 
