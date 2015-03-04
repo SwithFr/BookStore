b837d11cf0d72d2c94855cc2a32d18776a868dd8
Jeremy Smith <contact@swith.fr>
2015-03-04 17:43:02 +0100

    add nav element

---
+++ views/elements/nav.php
@@ -0,0 +1,13 @@
+<nav id="nav" class="nav">
+    <div class="nav__content">
+        <h1 class="nav__title"><a id="main__title" href="/">Book Store</a></h1>
+        <ul class="nav__menu">
+            <li class="nav__menu__item"><a href="./views/books/authorsList.html">Auteurs</a></li>
+            <li class="nav__menu__item"><a href="./views/books/editorsList.html">Editeurs</a></li>
+            <li class="nav__menu__item"><a href="./views/books/categoriesList.html">Genres</a></li>
+            <li class="nav__menu__item"><a href="./views/books/languagesList.html">Langues</a></li>
+            <li class="nav__menu__item"><a href="./views/books/yearsList.html">Années</a></li>
+            <li class="nav__menu__item"><a href="./views/books/yearsList.html">Bibliothèques</a></li>
+        </ul>
+    </div>
+</nav>
