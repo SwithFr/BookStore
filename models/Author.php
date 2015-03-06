50acbf9034970a9e9c96ea68795c21303a663308
Jeremy Smith <contact@swith.fr>
2015-03-06 18:00:09 +0100

    add getPopular

--- models/Author.php
+++ models/Author.php
@@ -3,5 +3,28 @@
 
 class Author extends AppModel
 {
+    /**
+     * Permet de récupérer les X Auteurs les mieux notés
+     * @param string $fields
+     * @param int $limit
+     * @return array
+     */
+    public function getPopular($fields, $limit)
+    {
+        $sql = 'SELECT ' . $fields . '
+                FROM authors
+                JOIN votes ON ref_id = authors.id
+                JOIN author_book ON author_id = authors.id
+                JOIN books ON book_id = books.id
+                WHERE ref = \'authors\'
+                ORDER BY value DESC
+                LIMIT ' . $limit;
+        var_dump($sql);die();
+        $pdost = $this->db->query($sql);
 
+        if ($limit > 1)
+            return $pdost->fetchAll();
+
+        return $pdost->fetch();
+    }
 } 
