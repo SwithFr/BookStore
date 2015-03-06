a6042c432ccb819af4ccc4aa61c4d1366a0edd8d
Jeremy Smith <contact@swith.fr>
2015-03-06 18:19:23 +0100

    affichage livres en vedette

--- views/books/index.php
+++ views/books/index.php
@@ -2,16 +2,17 @@
     <div class="section__header">
         <h2 class="section__title">En vedette :</h2><a href="./views/books/byAuthor.html" class="section__readMore">Voir tout le classement</a>
     </div>
-    <div class="section__block"><img src="./assets/img/book-img.jpg" class="section__block__img">
-        <h3 class="section__block__title">Un titre</h3>
-        <p class="section__block__author">Auteur</p>
-        <p class="section__block__content">Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus                   lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve.</p><a href="./views/books/single.html" class="section__readMore">En voir plus</a>
-    </div>
-    <div class="section__block"><img src="./assets/img/book-img.jpg" class="section__block__img">
-        <h3 class="section__block__title">Un titre</h3>
-        <p class="section__block__author">Auteur</p>
-        <p class="section__block__content">Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus                   lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve.</p><a href="./views/books/single.html" class="section__readMore">En voir plus</a>
-    </div>
+    <?php foreach($data['books'] as $book): ?>
+        <div class="section__block">
+            <img src="./assets/img/<?= $book->img; ?>" class="section__block__img">
+            <h3 class="section__block__title"><?= $book->title; ?></h3>
+            <p class="section__block__author"><?= $book->first_name . ' ' . $book->last_name; ?></p>
+            <p class="section__block__content">
+                <?= $book->summary; ?>
+            </p>
+            <a href="./views/books/single.html" class="section__readMore">En voir plus</a>
+        </div>
+    <?php endforeach; ?>
 </div>
 <div class="section author">
     <div class="section__header">
