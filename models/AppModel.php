42cb9bd8e63b2e451f5d7ea67bcdc8172024b058
Jeremy Smith <contact@swith.fr>
2015-03-06 17:09:20 +0100

    add function get

--- models/AppModel.php
+++ models/AppModel.php
@@ -22,4 +22,64 @@ class AppModel
         
         $this->table = strtolower(get_class($this)) . 's';
     }
+
+    public function get(array $conditions = null, $table = null)
+    {
+
+        $query = "SELECT ";
+
+        // Si on a des champs définis
+        if (!isset($conditions['fields']))
+            $query .= "*";
+        else
+            $query .= $conditions['fields'];
+
+
+        if (is_null($table)) {
+            $query .= " FROM " . $this->table;
+        } else {
+            $query .= " FROM " . $table;
+        }
+
+        // Si on a un Where
+        if (isset($conditions['where'])) {
+            if (!is_array($conditions['where'])) {
+                $query .= " WHERE " . $conditions['where'];
+            } else {
+                $query .= " WHERE ";
+                $cond = [];
+                foreach ($conditions['where'] as $k => $v) {
+                    if (!is_numeric($v))
+                        $v = "'" . addslashes($v) . "'";
+                    $cond[] = "$k=$v";
+                }
+                $query .= implode(' AND ', $cond);
+            }
+        }
+
+        // Si on a une limite
+        if (isset($conditions['limit'])) {
+            if (isset($conditions['offset'])) {
+                $query .= " LIMIT " . $conditions['offset'] . "," . $conditions['limit'];
+            } else {
+                $query .= " LIMIT " . $conditions['limit'];
+            }
+
+        }
+
+        // Si on a un group by
+        if (isset($conditions['groupBy'])) {
+            $query .= " GROUP BY " . $conditions['groupBy'];
+        }
+
+        // Si on a un order
+        if (isset($conditions['order'])) {
+            $query .= " ORDER BY " . $conditions['order'];
+        }
+
+        // debug($query);
+        $req = $this->bdd->query($query);
+
+        return $req->fetchAll();
+    }
 } 
