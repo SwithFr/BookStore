b6ca162ddf488ed132964277b340e5c30a749b4e
Jeremy Smith <contact@swith.fr>
2015-03-06 18:26:05 +0100

    affichage auteur mieu noté

--- views/books/index.php
+++ views/books/index.php
@@ -1,6 +1,6 @@
 <div class="section vedettes">
     <div class="section__header">
-        <h2 class="section__title">En vedette :</h2><a href="./views/books/byAuthor.html" class="section__readMore">Voir tout le classement</a>
+        <h2 class="section__title">Livres les mieux notés :</h2><a href="./views/books/byAuthor.html" class="section__readMore">Voir tout le classement</a>
     </div>
     <?php foreach($data['books'] as $book): ?>
         <div class="section__block">
@@ -16,14 +16,16 @@
 </div>
 <div class="section author">
     <div class="section__header">
-        <h2 class="section__title"><a href="./views/books/byAuthor.html">Un auteur au hasard</a></h2><a href="./views/books/byAuthor.html" class="section__readMore">Voir plus du même auteur</a>
+        <h2 class="section__title"><a href="./views/books/byAuthor.html">Auteur le mieux noté</a></h2><a href="./views/books/byAuthor.html" class="section__readMore">Voir plus du même auteur</a>
     </div>
     <div class="section__block">
-        <h3 class="section__block__title">Nom de l'auteur</h3>
-        <p class="section__block__year">1700 - 1800</p>
-        <p class="section__block__content">Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus                   lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve. Ipsum vitae. Lacus netus mauris lorem montes enim cubilia urna dolor cubilia elementum. Purus ipsum nulla conubia et pharetra ante mus lorem taciti venenatis inceptos eu scelerisque ve.</p>
+        <h3 class="section__block__title"><?= $data['author']->first_name . ' ' . $data['author']->last_name; ?></h3>
+        <p class="section__block__year"><?= $data['author']->date_birth; ?> - <?= $data['author']->date_death; ?></p>
+        <p class="section__block__content">
+            <?= $data['author']->bio; ?>
+        </p>
         <div class="author__count">
-            <p class="nb author__nbBooks">20</p>
+            <p class="nb author__nbBooks"><?= $data['author']->nb_livres; ?></p>
             <p class="nb author__eval">4.5</p>
         </div>
     </div>
