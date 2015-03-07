d7139765cad30a40da1ce3a4f4abf5ddbee42415
Jeremy Smith <contact@swith.fr>
2015-03-07 17:51:03 +0100

    update function index

--- controllers/EditorsController.php
+++ controllers/EditorsController.php
@@ -3,8 +3,17 @@
 
 class EditorsController extends AppController
 {
+    /**
+     * PAGE LISTE EDITEURS
+     */
     public function index()
     {
+        if (isset($_GET['letter']) && !empty($_GET['letter']) && preg_match('/[A-Z]/', ucfirst($_GET['letter']))) {
+            $letter = $_GET['letter'];
+            $editors = $this->Editor->getAllFromLetter("*", 'name', $letter);
+        } else
+            header("Location: " . $_SERVER['PHP_SELF'] . '?a=index&e=editor&letter=a');
 
+        return compact("editors", "letter");
     }
 } 
