8acf3292c14be086d253e7134eda31d56b98f65a
Jeremy Smith <contact@swith.fr>
2015-03-07 18:05:44 +0100

    add editors-form element

---
+++ views/elements/editors-form.php
@@ -0,0 +1,4 @@
+<form class="form">
+    <input placeholder="Chercher un editeur..." class="form__input form--search">
+    <input type="submit" class="form__input form__submit form--search--submit">
+</form>
