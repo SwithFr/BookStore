afef3b65c7797ad072781f7b32bf6b89ee429894
Jeremy Smith <contact@swith.fr>
2015-03-07 17:50:12 +0100

    update url

--- views/elements/nav.php
+++ views/elements/nav.php
@@ -1,6 +1,6 @@
 <nav id="nav" class="nav">
     <div class="nav__content">
-        <h1 class="nav__title"><a id="main__title" href="/">Book Store</a></h1>
+        <h1 class="nav__title"><a id="main__title" href="<?= $_SERVER['PHP_SELF']; ?>">Book Store</a></h1>
         <ul class="nav__menu">
             <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=author&letter=a">Auteurs</a></li>
             <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=editor&letter=a">Editeurs</a></li>
