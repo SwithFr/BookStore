3c7125551dd73e09244d9163ffc945114c5d3b03
Jeremy Smith <contact@swith.fr>
2015-03-07 16:49:14 +0100

    add variable $letter

--- controllers/AuthorsController.php
+++ controllers/AuthorsController.php
@@ -8,12 +8,13 @@ class AuthorsController extends AppController
      */
     public function index()
     {
-        if (isset($_GET['letter']) && !empty($_GET['letter']) && preg_match('/[A-Z]/',ucfirst($_GET['letter'])))
-            $authors = $this->Author->getAllFromLetter("*",$_GET['letter']);
-        else 
-            header("Location: "  . $_SERVER['PHP_SELF'] . '?a=index&e=author&letter=A');
+        if (isset($_GET['letter']) && !empty($_GET['letter']) && preg_match('/[A-Z]/', ucfirst($_GET['letter']))) {
+            $letter = $_GET['letter'];
+            $authors = $this->Author->getAllFromLetter("*", $letter);
+        } else
+            header("Location: " . $_SERVER['PHP_SELF'] . '?a=index&e=author&letter=A');
 
-        return compact("authors");
+        return compact("authors","letter");
     }
 
 } 
