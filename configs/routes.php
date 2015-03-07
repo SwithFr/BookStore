22a8ca958a0051a9ec7ab51b0c1e50f41ede6e83
Jeremy Smith <contact@swith.fr>
2015-03-07 15:26:16 +0100

    add routes for nav

--- configs/routes.php
+++ configs/routes.php
@@ -1,5 +1,10 @@
 <?php
 
 return $routes = [
-    'default' => 'index/book'
+    'default'    => 'index/book',
+    'A_index'    => 'index/author',
+    'E_index'    => 'index/editor',
+    'G_index'    => 'index/genre',
+    'B_populars' => 'populars/book',
+    'L_index'    => 'index/librarie'
 ];
