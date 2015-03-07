66a1d2e52624e337842dceee0887df30006eeb27
Jeremy Smith <contact@swith.fr>
2015-03-07 17:42:35 +0100

    fix syntaxe

--- controllers/BooksController.php
+++ controllers/BooksController.php
@@ -9,13 +9,13 @@ class BooksController extends AppController
     public function index()
     {
         # Les 2 livres les mieux notés
-        $books = $this->Book->getPopular('books.id,title,first_name,last_name,summary,books.img',2);
+        $books = $this->Book->getPopular('books.id,title,first_name,last_name,summary,books.img', 2);
 
         # l'auteur le mieux noté
         $this->loadModel('Author');
-        $author = $this->Author->getPopular('first_name,last_name,bio,nb_livres,date_birth,date_death',1);
+        $author = $this->Author->getPopular('first_name,last_name,bio,nb_livres,date_birth,date_death', 1);
 
-        return compact("books","author");
+        return compact("books", "author");
     }
 
 } 
