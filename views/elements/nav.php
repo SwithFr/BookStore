9431aa924eb1516d97158f7e885f1a29f0416041
Jeremy Smith <contact@swith.fr>
2015-03-07 15:22:38 +0100

    update nav

--- views/elements/nav.php
+++ views/elements/nav.php
@@ -2,10 +2,10 @@
     <div class="nav__content">
         <h1 class="nav__title"><a id="main__title" href="/">Book Store</a></h1>
         <ul class="nav__menu">
-            <li class="nav__menu__item"><a href="<?= BASE; ?>/book/authors">Auteurs</a></li>
-            <li class="nav__menu__item"><a href="<?= BASE; ?>/book/editors">Editeurs</a></li>
-            <li class="nav__menu__item"><a href="<?= BASE; ?>/book/genres">Genres</a></li>
-            <li class="nav__menu__item"><a href="<?= BASE; ?>/book/populars">Langues</a></li>
+            <li class="nav__menu__item"><a href="<?= BASE; ?>/author/index">Auteurs</a></li>
+            <li class="nav__menu__item"><a href="<?= BASE; ?>/editor/index">Editeurs</a></li>
+            <li class="nav__menu__item"><a href="<?= BASE; ?>/genre/index">Genres</a></li>
+            <li class="nav__menu__item"><a href="<?= BASE; ?>/book/populars">Classement</a></li>
             <li class="nav__menu__item"><a href="<?= BASE; ?>/library/index">Bibliothèques</a></li>
         </ul>
     </div>
