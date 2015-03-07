dc56316cf316e494ca0968beed6e0f3dca660ffc
Jeremy Smith <contact@swith.fr>
2015-03-07 17:38:00 +0100

    update url

--- views/elements/nav.php
+++ views/elements/nav.php
@@ -2,8 +2,8 @@
     <div class="nav__content">
         <h1 class="nav__title"><a id="main__title" href="/">Book Store</a></h1>
         <ul class="nav__menu">
-            <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=author&letter=A">Auteurs</a></li>
-            <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=editor">Editeurs</a></li>
+            <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=author&letter=a">Auteurs</a></li>
+            <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=editor&letter=a">Editeurs</a></li>
             <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=genre">Genres</a></li>
             <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=popular&e=book">Classement</a></li>
             <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=library">Bibliothèques</a></li>
