5aae68dc351d3677877b1a9cf4580c7b70337d1c
Jeremy Smith <contact@swith.fr>
2015-03-07 16:20:10 +0100

    add authors/index function

--- controllers/AuthorsController.php
+++ controllers/AuthorsController.php
@@ -8,7 +8,12 @@ class AuthorsController extends AppController
      */
     public function index()
     {
-        echo("Liste auteurs");
+        if (isset($_GET['letter']) && !empty($_GET['letter']) && preg_match('/[A-Z]/',ucfirst($_GET['letter'])))
+            $authors = $this->Author->getAllFromLetter("*",$_GET['letter']);
+        else 
+            header("Location: "  . $_SERVER['PHP_SELF'] . '?a=index&e=author&letter=A');
+
+        return compact("authors");
     }
 
 } 
