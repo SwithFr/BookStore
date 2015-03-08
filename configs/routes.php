6b34d0a0bf38de82f4e913f89420df118d435e31
Jeremy Smith <contact@swith.fr>
2015-03-08 10:59:11 +0100

    add disconnect route

--- configs/routes.php
+++ configs/routes.php
@@ -1,11 +1,12 @@
 <?php
 
 return $routes = [
-    'default'    => 'index/book',
-    'A_index'    => 'index/author',
-    'E_index'    => 'index/editor',
-    'G_index'    => 'index/genre',
-    'B_populars' => 'populars/book',
-    'L_index'    => 'index/librarie',
-    'U_check'    => 'check/user'
+    'default'      => 'index/book',
+    'A_index'      => 'index/author',
+    'E_index'      => 'index/editor',
+    'G_index'      => 'index/genre',
+    'B_populars'   => 'populars/book',
+    'L_index'      => 'index/librarie',
+    'U_check'      => 'check/user',
+    'U_disconnect' => 'disconnect/user'
 ];
