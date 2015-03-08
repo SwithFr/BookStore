956514e474c7d9af161d88f88d36a6389d334714
Jeremy Smith <contact@swith.fr>
2015-03-08 10:41:54 +0100

    add functions check, connect and disconnect

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -2,5 +2,35 @@
 
 class UsersController extends AppController
 {
+    public function check()
+    {
+        if ($this->request->method === "POST") {
+            $v = new Validator();
+            if (!$v->validate($_POST, $this->User->rules))
+                die('Pas bon :p');
+            $user = $this->User->getLogged($_POST['login']);
+            if ($user->password != $_POST['password']) {
+                die('Identifiants inconnus');
+            } else {
+                $this->connect($user);
+            }
+        }
+    }
 
+    private function connect(User $user)
+    {
+        $_SESSION['id'] = $user->id;
+        $_SESSION['role'] = $user->role;
+        setcookie("user_id", $user->id, time() + 7 * 24 * 3600);
+//        header("Location: " . $_SERVER['PHP_SELF']);
+    }
+
+    public function disconnect()
+    {
+        session_destroy();
+        unset($_SESSION['id']);
+        unset($_SESSION['role']);
+        setcookie("user_id", "", time() - 7 * 24 * 3600);
+//        header('Location: ' . $_SERVER['PHP_SELF']);
+    }
 } 
