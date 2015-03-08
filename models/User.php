256e01ee7658ad9f35a22212aba7bc1d460ac161
Jeremy Smith <contact@swith.fr>
2015-03-08 10:43:04 +0100

    add rules and getLogged function

--- models/User.php
+++ models/User.php
@@ -3,5 +3,31 @@
 
 class User extends AppModel
 {
+    public $rules = [
+        'login' => [
+            ['ruleName'=>'notEmpty', 'message'=>'Le champ login est obligatoire'],
+            ['ruleName'=>'isString','message'=>'Doit être une chaine de caractère'],
+        ],
+        'password' => [
+            ['ruleName'=>'notEmpty', 'message'=>'Le champ mot de passe est obligatoire']
+        ],
+        'mail' => [
+            ['ruleName'=>'isMail', 'message'=>'Doit être un email valide'],
+            ['ruleName'=>'notEmpty', 'message'=>'Le champ email est obligatoire']
+        ]
+    ];
 
+    /**
+     * Permet de récuperer les infos en bdd pour vérifier si l'utilisateur à bien entré un bon login/mdp
+     * @param  string $login Le login entré par l'utilisateur
+     * @return stdClass      un objet content les indos trouvées.
+     */
+    public function getLogged($login)
+    {
+        $sql = 'SELECT id,password,role FROM users WHERE login=\':login\'';
+        $pdost = $this->db->prepare($sql);
+        $pdost->execute([':login'=>$login]);
+
+        return $pdost->fetch();
+    }
 } 
