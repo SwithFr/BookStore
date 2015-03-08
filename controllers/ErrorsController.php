40ee9dcff8a4b73d83607b26455819d111343602
Jeremy Smith <contact@swith.fr>
2015-03-08 22:03:55 +0100

    add function notLogged

--- controllers/ErrorsController.php
+++ controllers/ErrorsController.php
@@ -3,10 +3,24 @@
 
 class ErrorsController extends AppController
 {
+    public $layout = 'error';
+    /**
+     * Fonction d'erreur lorsque l'on tente d'accèder à une page non permise
+     * @return array
+     */
     public function unauthorized()
     {
-        $this->layout = 'error';
         $message = "Hey bonhomme ! Je te conseille de pas aller plus loin...";
+        return compact('message');
+    }
+
+    /**
+     * Fonction d'erreur lorsqu'on tente d'accèder à une page reservée sans être connecté
+     * @return array
+     */
+    public function notLogged()
+    {
+        $message = "Petit malin ! Tu n'es pas connecté...";
         return compact('message');
     }
 
