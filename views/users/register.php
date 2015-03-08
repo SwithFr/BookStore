fee8e742057a1d2503b9c293964ae24cf35a9177
Jeremy Smith <contact@swith.fr>
2015-03-08 11:59:27 +0100

    fix syntaxe

--- views/users/register.php
+++ views/users/register.php
@@ -1,13 +1,13 @@
 <div class="section">
     <form action="<?= Html::url('register','user'); ?>" method="post">
         <label for="login">Login</label>
-        <?= isset($data['errors']['login']) ? $data['errors']['login'] : ''; ?>
+        <span><?= isset($data['errors']['login']) ? $data['errors']['login'] : ''; ?></span>
         <input type="text" name="login" id="login" value="<?= isset($_POST['login']) ? $_POST['login'] : ''; ?>"/>
         <label for="email">Email</label>
-        <?= isset($data['errors']['email']) ? $data['errors']['email'] : ''; ?>
+        <span><?= isset($data['errors']['email']) ? $data['errors']['email'] : ''; ?></span>
         <input type="email" name="email" id="email" value="<?= isset($_POST['email']) ? $_POST['email'] : ''; ?>"/>
         <label for="password">Mot de passe</label>
-        <?= isset($data['errors']['password']) ? $data['errors']['password'] : ''; ?>
+        <span><?= isset($data['errors']['password']) ? $data['errors']['password'] : ''; ?></span>
         <input type="password" name="password" id="password"/>
         <input type="submit" value="Se connecter"/>
     </form>
