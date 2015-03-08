5d34aca8a3d90297dd29f49119810aff55a45a00
Jeremy Smith <contact@swith.fr>
2015-03-08 12:00:22 +0100

    add errors

--- views/users/check.php
+++ views/users/check.php
@@ -1,8 +1,10 @@
 <div class="section">
     <form action="<?= Html::url('check','user'); ?>" method="post">
         <label for="login">Login</label>
-        <input type="text" name="login" id="login"/>
+        <span><?= isset($data['errors']['login']) ? $data['errors']['login'] : ''; ?></span>
+        <input type="text" name="login" id="login" value="<?= isset($_POST['login']) ? $_POST['login'] : ''; ?>"/>
         <label for="password">Mot de passe</label>
+        <span><?= isset($data['errors']['password']) ? $data['errors']['password'] : ''; ?></span>
         <input type="password" name="password" id="password"/>
         <label for="remember">Se souvenir de moi</label>
         <input type="checkbox" name="remember" id="remember"/>
