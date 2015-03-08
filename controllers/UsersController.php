ab7d5d59622734b560e490e51eeca6f3417044e4
Jeremy Smith <contact@swith.fr>
2015-03-08 11:38:11 +0100

    add flash messages and register function

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -9,13 +9,15 @@ class UsersController extends AppController
     {
         if ($_SERVER['REQUEST_METHOD'] === "POST" && !isset($_COOKIE['user_id'])) {
             $v = new Validator();
-            if (!$v->validate($_POST, $this->User->rules))
-                die('Pas bon :p');
+            if (!$v->validate($_POST, $this->User->rules)) {
+                Session::setFlash("Verifiez vos informations !", 'error');
+                return compact($v->errors());
+            }
             $user = $this->User->getLogged($_POST['login']);
             if ($user->password != $_POST['password']) {
-                die('Identifiants inconnus');
+                Session::setFlash("Identifiants inconnus !", 'error');
             } else {
-                $this->connect($user,$_POST['remember']);
+                $this->connect($user, $_POST['remember']);
             }
         }
     }
@@ -25,7 +27,7 @@ class UsersController extends AppController
      * @param $user
      * @param $remember
      */
-    private function connect($user,$remember)
+    private function connect($user, $remember)
     {
         $_SESSION['user_id'] = $user->id;
         $_SESSION['user_role'] = $user->role;
@@ -33,6 +35,7 @@ class UsersController extends AppController
             setcookie("user_id", $user->id, time() + 7 * 24 * 3600);
             setcookie("user_role", $user->role, time() + 7 * 24 * 3600);
         }
+        Session::setFlash('Vous êtes maintenant connecté.');
         header("Location: " . $_SERVER['PHP_SELF']);
     }
 
@@ -46,6 +49,25 @@ class UsersController extends AppController
         unset($_SESSION['user_role']);
         setcookie("user_id", "", time() - 7 * 24 * 3600);
         setcookie("user_role", "", time() - 7 * 24 * 3600);
+        Session::setFlash("Vous êtes bien déconnecté !");
         header('Location: ' . $_SERVER['PHP_SELF']);
+    }
+
+    /**
+     * Formulaire d'enregistrement
+     */
+    public function register()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === "POST" && !isset($_COOKIE['user_id'])) {
+            $v = new Validator();
+            if (!$v->validate($_POST, $this->User->rules)) {
+                Session::setFlash("Verifiez vos informations !", 'error');
+                $errors = $v->errors();
+                return compact("errors");
+            }
+            $this->User->create($_POST['login'], $_POST['password'], $_POST['email']);
+            Session::setFlash("Votre compte a bien été créé");
+            header('Location: ' . Html::url('check', 'user'));
+        }
     }
 } 
