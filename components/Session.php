8f01fc5cfce5ce5bbbe479cc00060b542e52d608
Jeremy Smith <contact@swith.fr>
2015-03-08 11:37:33 +0100

    add flash functions

--- components/Session.php
+++ components/Session.php
@@ -3,5 +3,26 @@
 
 class Session 
 {
+    /**
+     * Permet de définir un message flash
+     * @param string $message le message à afficher
+     * @param string $type le type de message [success]
+     */
+    public static function setFlash($message, $type = "success")
+    {
+        $_SESSION['flash']['message'] = $message;
+        $_SESSION['flash']['type'] = $type;
+    }
 
+    /**
+     * Permet d'afficher un message flash si il est défini
+     * @return string
+     */
+    public static function flash()
+    {
+        if (isset($_SESSION['flash'])) {
+            echo "<a href='#' class='alert alert-" . $_SESSION['flash']['type'] . "'>" . $_SESSION['flash']['message'] . "</a>";
+            unset($_SESSION['flash']);
+        }
+    }
 } 
