e4280063876f4c215e5db5f928222e451d53f154
Jeremy Smith <contact@swith.fr>
2015-03-09 14:52:33 +0100

    add function add

--- controllers/LibrariesController.php
+++ controllers/LibrariesController.php
@@ -3,5 +3,25 @@
 
 class LibrariesController extends AppController
 {
+    /**
+     * Formulaire d'ajout de librairie
+     */
+    public function add()
+    {
+        if (!Session::isLogged())
+            header('Location: ' . Html::url('notLogged', 'error'));
 
-} 
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $v = new Validator();
+            if (!$v->validate($_POST, $this->Librarie->rules)) {
+                Session::setFlash("Verifiez vos informations !", 'error');
+                $errors = $v->errors();
+                return compact("errors");
+            }
+            $private = isset($_POST['private']) ? true : false;
+            $this->Librarie->create($_POST['name'], $_POST['address'], $_POST['tel'], $_POST['email'], $_SESSION['user_id'], $private);
+            Session::setFlash('La bibliothèque ' . $_POST['name'] . 'a bien été ajoutée !');
+            $this->redirect('account','user');
+        }
+    }
+}
