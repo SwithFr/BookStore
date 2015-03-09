56588841cc7073a9dc2791d0c3ff6cb199e32b8a
Jeremy Smith <contact@swith.fr>
2015-03-09 14:46:43 +0100

    add function create

--- models/Librarie.php
+++ models/Librarie.php
@@ -3,13 +3,33 @@
 
 class Librarie extends AppModel
 {
-    protected $table = 'libraries';
+    public $table = 'libraries';
 
-    public function create($name, $adress, $tel, $email, $user_id)
+    public $rules = [
+        'name' => [
+            ['ruleName' => 'notEmpty', 'message' => 'Ce champ est obligatoire'],
+            ['ruleName' => 'isString', 'message' => 'Doit être une chaine de caractère']
+        ],
+        'email' => [
+            ['ruleName' => 'notEmpty', 'message' => 'Ce champ est obligatoire'],
+            ['ruleName' => 'isMail', 'message' => 'Doit être un email valide']
+        ]
+    ];
+
+    /**
+     * Ajoute une librarie en base de données
+     * @param $name
+     * @param $adress
+     * @param $tel
+     * @param $email
+     * @param $user_id
+     * @param $private
+     */
+    public function create($name, $adress, $tel, $email, $user_id, $private)
     {
-        $sql = 'INSERT INTO libraries(name,address,tel,email,user_id) VALUES (:name,:address,:tel,:email,:user_id)';
+        $sql = 'INSERT INTO libraries(name,address,tel,email,user_id,private) VALUES (:name,:address,:tel,:email,:user_id,:private)';
         $pdost = $this->db->prepare($sql);
-        $pdost->execute([':name' => $name, ':address' => $adress, ':tel' => $tel, ':email' => $email, ':user_id' => $user_id]);
+        $pdost->execute([':name' => $name, ':address' => $adress, ':tel' => $tel, ':email' => $email, ':user_id' => $user_id, ':private' => intval($private)]);
     }
 
 } 
