be0e2adfe81e75fbd6c5a59b85f63efa884f26e9
Jeremy Smith <contact@swith.fr>
2015-03-09 18:28:12 +0100

    add validation rules

--- models/Book.php
+++ models/Book.php
@@ -3,6 +3,23 @@
 
 class Book extends AppModel
 {
+    public $rules = [
+        'title' => [
+            ['ruleName' => 'notEmpty', 'message' => 'Le titre est obligatoire'],
+            ['ruleName' => 'isString', 'message' => 'Le titre doit être une chaine de caractère']
+        ],
+        'summary' => [
+            ['ruleName' => 'notEmpty', 'message' => 'Le résumé est obligatoire']
+        ],
+        'isbn' => [
+            ['ruleName' => 'notEmpty', 'message' => 'Le champ email est obligatoire']
+        ],
+        'nbpages' => [
+            ['ruleName' => 'notEmpty', 'message' => 'Le nombre de pages est obligatoire'],
+            ['ruleName' => 'isInt', 'message' => 'Ceci n‘est pas un nombre']
+        ]
+    ];
+
     /**
      * Permet de récupérer les X livres les mieux notés
      * @param string $fields
@@ -47,7 +64,7 @@ class Book extends AppModel
 
         if (!is_null($limit))
             $sql .= 'LIMIT ' . $limit;
-        
+
         $pdost = $this->db->query($sql);
 
         if ($limit > 1 || is_null($limit))
