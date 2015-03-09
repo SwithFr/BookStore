bbdd6282693e7cd4a4a7974911318aa0af248c1f
Jeremy Smith <contact@swith.fr>
2015-03-09 15:56:48 +0100

    update view users/account

--- views/users/account.php
+++ views/users/account.php
@@ -1,54 +1,37 @@
 <div id="content" class="content">
-    <div class="section dashbord user">
+    <div class="section section--dashbord">
         <div class="section__header">
-            <h2 class="section__title">Dashboard <span class="user__login"><?= $data['user']->login; ?></span></h2>
+            <h2 class="section__title">Bienvenue sur votre Dashboard <span class="user__login"><?= $data['user']->login; ?></span></h2>
         </div>
-        <?php if(!$data['library']): ?>
+        <?php if(!$data['hasLibrary']): ?>
             <p class="alert alert--warning">
                 Veuillez d'abord créer une bibliothèque pour pouvoir ajouter des livres.
             </p>
             <a class="btn btn--add" href="<?= Html::url('add','librarie'); ?>">Créer une bibliothèque</a>
         <?php else: ?>
-            <div class="section__block">
-                <h3 class="section__block__title">Mes livres ajoutés</h3>
-                <ul class="books__list">
-                    <li class="books__list__item">Titre du livre
-                        <div class="actions"><a href="#">Edit<i class="icon-pencil"></i></a><a href="#" class="admin--delete">Suppr<i class="icon-cancel"></i></a></div>
-                    </li>
-                    <li class="books__list__item">Titre du livre
-                        <div class="actions"><a href="#">Edit<i class="icon-pencil"></i></a><a href="#" class="admin--delete">Suppr<i class="icon-cancel"></i></a></div>
-                    </li>
-                    <li class="books__list__item">Titre du livre
-                        <div class="actions"><a href="#">Edit<i class="icon-pencil"></i></a><a href="#" class="admin--delete">Suppr<i class="icon-cancel"></i></a></div>
-                    </li>
-                    <li class="books__list__item">Titre du livre
-                        <div class="actions"><a href="#">Edit<i class="icon-pencil"></i></a><a href="#" class="admin--delete">Suppr<i class="icon-cancel"></i></a></div>
-                    </li>
-                    <li class="books__list__item">Titre du livre
-                        <div class="actions"><a href="#">Edit<i class="icon-pencil"></i></a><a href="#" class="admin--delete">Suppr<i class="icon-cancel"></i></a></div>
-                    </li>
-                    <li class="books__list__item">Titre du livre
-                        <div class="actions"><a href="#">Edit<i class="icon-pencil"></i></a><a href="#" class="admin--delete">Suppr<i class="icon-cancel"></i></a></div>
-                    </li>
-                    <li class="books__list__item">Titre du livre
-                        <div class="actions"><a href="#">Edit<i class="icon-pencil"></i></a><a href="#" class="admin--delete">Suppr<i class="icon-cancel"></i></a></div>
-                    </li>
-                    <li class="books__list__item">Titre du livre
-                        <div class="actions"><a href="#">Edit<i class="icon-pencil"></i></a><a href="#" class="admin--delete">Suppr<i class="icon-cancel"></i></a></div>
-                    </li>
-                </ul>
-            </div>
-            <div class="section__block">
-                <h3 class="section__block__title">Ajouter un livre</h3>
-                <form>
-                    <input type="text" placeholder="Titre du livre" class="form__addBook__input">
-                    <input type="text" placeholder="Année de publication" class="form__addBook__input">
-                    <input type="text" placeholder="Genre" class="form__addBook__input">
-                    <input type="text" placeholder="Auteur" class="form__addBook__input">
-                    <textarea placeholder="Résumé" class="form__addBook__input"></textarea>
-                    <input type="file" class="form__addBook__input">
-                    <input type="submit" value="Ajouter" class="form__addBook__input">
-                </form>
+            <div class="section__block section__block--library">
+                <h3 class="section__block__title section__block__title--library"><?= $data['library']->name; ?></h3>
+                <table class="books__list">
+                    <thead>
+                        <tr>
+                            <th>Titre du livre</th>
+                            <th>Auteur</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($data['books'] as $book): ?>
+                            <tr>
+                                <td class="books__list__item"><?= $book->title; ?></td>
+                                <td><?= $book->first_name . ' ' . $book->last_name; ?></td>
+                                <td class="actions">
+                                    <a href="#">Edit<i class="icon-pencil"></i></a>
+                                    <a href="#" class="admin--delete">Suppr<i class="icon-cancel"></i></a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
             </div>
         <?php endif; ?>
     </div>
