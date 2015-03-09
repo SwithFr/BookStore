58e24df1f43833e5097d03429ea64e8354d69fe4
Jeremy Smith <contact@swith.fr>
2015-03-09 17:47:41 +0100

    add function getAllFromUserLibrary

--- models/Location.php
+++ models/Location.php
@@ -3,5 +3,16 @@
 
 class Location extends AppModel
 {
+    public function getAllFromUserLibrary($user_id)
+    {
+        $sql = 'SELECT locations.id, locations.name
+                FROM locations
+                JOIN location_library ON location_id = locations.id
+                JOIN libraries ON library_id = libraries.id
+                WHERE libraries.user_id = :user_id';
+        $pdost = $this->db->prepare($sql);
+        $pdost->execute([':user_id' => $user_id]);
 
+        return $pdost->fetchAll();
+    }
 } 
