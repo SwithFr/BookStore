503be2863802a3a57884f943849dbb93ab707c43
Jeremy Smith <contact@swith.fr>
2015-03-09 10:03:59 +0100

    change property rules to protected

--- models/User.php
+++ models/User.php
@@ -3,7 +3,7 @@
 
 class User extends AppModel
 {
-    public $rules = [
+    protected  $rules = [
         'login' => [
             ['ruleName' => 'notEmpty', 'message' => 'Le champ login est obligatoire'],
             ['ruleName' => 'isString', 'message' => 'Doit être une chaine de caractère'],
