c7b9c06994161c59724fe30179a879129528fd8d
Jeremy Smith <contact@swith.fr>
2015-03-10 13:55:22 +0100

    add function add

--- controllers/EditorsController.php
+++ controllers/EditorsController.php
@@ -2,6 +2,10 @@
 
 namespace Controllers;
 
+use Components\Session;
+use Components\Validator;
+use Helpers\Image;
+
 class EditorsController extends AppController
 {
     /**
@@ -13,8 +17,32 @@ class EditorsController extends AppController
             $letter = $_GET['letter'];
             $editors = $this->Editor->getAllFromLetter("*", 'name', $letter);
         } else
-            $this->redirect('index','author',['letter'=>'a']);
+            $this->redirect('index', 'author', ['letter' => 'a']);
 
         return compact("editors", "letter");
+    }
+
+    public function add()
+    {
+        if (!Session::isLogged())
+            $this->redirect('notLogged', 'error');
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $v = new Validator();
+            if (!$v->validate($_POST, $this->Editor->rules)) {
+                Session::setFlash('Veuillez vérifier vos informations', 'error');
+                $errors = $v->errors();
+                return compact('errors');
+            }
+
+            $name = time() . '.' . pathinfo($_FILES['img']['name'], PATHINFO_EXTENSION);
+            $dest = D_ASSETS . DS . 'img' . DS . 'uploads' . DS . 'editors' . DS;
+            if (!empty($_FILES))
+                Image::uploadImg($dest, $name);
+
+            $this->Editor->create($_POST['name'], $_POST['website'], $dest . $name, $_POST['history']);
+            Session::setFlash('L‘éditeur ' . $_POST['first_name'] . ' ' . $_POST['last_name'] . ' a bien été ajouté !');
+            $this->redirect('add', 'book');
+        }
     }
 } 
