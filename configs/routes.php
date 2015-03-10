84088b30268505dd8dad163a4d03a5a513628599
Jeremy Smith <contact@swith.fr>
2015-03-10 14:06:11 +0100

    add routes comments

--- configs/routes.php
+++ configs/routes.php
@@ -2,20 +2,35 @@
 
 return $routes = [
     'default'         => 'index/book',
+    # Routes auteurs
     'A_index'         => 'index/author',
     'A_add'           => 'add/author',
+
+    # Routes éditeurs
     'E_index'         => 'index/editor',
     'E_add'           => 'add/editor',
+
+    # Routes genres
     'G_index'         => 'index/genre',
+
+    # Routes livres
     'B_populars'      => 'populars/book',
     'B_add'           => 'add/book',
+
+    # Routes bibliothèques
     'L_index'         => 'index/librarie',
     'L_add'           => 'add/librarie',
+
+    # Routes utilisateurs
     'U_check'         => 'check/user',
     'U_disconnect'    => 'disconnect/user',
     'U_register'      => 'register/user',
     'U_account'       => 'account/user',
+
+    # Routes emplacements
     'Lo_add'          => 'add/location',
+
+    # Routes erreurs
     'Er_unauthorized' => 'unauthorized/error',
     'Er_notLogged'    => 'notLogged/error'
 ];
