7d702e428ef4cfc64af9f9bca5944e8a2daf633f
Jeremy Smith <contact@swith.fr>
2015-03-11 19:25:28 +0100

    add class

--- views/books/view.php
+++ views/books/view.php
@@ -5,7 +5,9 @@
             <div class="section__header">
                 <h2 class="section__title"><?= $data['book']->title; ?></h2>
                 <div class="section__block bio">
-                    <p class="section__block__year">Écrit par <a href="<?= Html::url('view','author',['id'=>$data['book']->a_id]); ?>"><?= $data['book']->first_name . ' ' . $data['book']->last_name; ?></a></p>
+                    <p class="section__block__year">
+                        Écrit par <a href="<?= Html::url('view','author',['id'=>$data['book']->a_id]); ?>"
+                                     class="author__name"><?= $data['book']->first_name . ' ' . $data['book']->last_name; ?></a></p>
                     <p class="section__block__category"><?= $data['book']->g_name; ?></p>
                     <div class="section__block__content">
                         <?= $data['book']->summary; ?>
