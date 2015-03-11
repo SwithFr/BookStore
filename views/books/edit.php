6f0289613adc02459da825e15d86c03921a35a42
Jeremy Smith <contact@swith.fr>
2015-03-11 15:22:40 +0100

    fix isset $d

--- views/books/edit.php
+++ views/books/edit.php
@@ -21,7 +21,7 @@
                type="text"
                name="title"
                id="title"
-               value="<?= $data['d']['title']; ?>"/>
+               value="<?= isset($data['d']['title']) ? $data['d']['title'] : ''; ?>"/>
 
         <label class="form__label" for="img">Image de couverture</label>
         <?= isset($data['errors']['img']) ? '<span class="has-error">' . $data['errors']['img'] . '</span>' : ''; ?></span>
@@ -34,7 +34,7 @@
         <?= isset($data['errors']['summary']) ? '<span class="has-error">' . $data['errors']['summary'] . '</span>' : ''; ?></span>
         <textarea class="form__textarea"
                   name="summary"
-                  id="summary"><?= $data['d']['summary']; ?></textarea>
+                  id="summary"><?= isset($data['d']['summary']) ? $data['d']['summary'] : ''; ?></textarea>
 
         <label class="form__label" for="isbn">Numéro ISBN</label>
         <?= isset($data['errors']['isbn']) ? '<span class="has-error">' . $data['errors']['isbn'] . '</span>' : ''; ?></span>
@@ -42,7 +42,7 @@
                type="text"
                name="isbn"
                id="isbn"
-               value="<?= $data['d']['isbn']; ?>"/>
+               value="<?= isset($data['d']['isbn']) ? $data['d']['isbn'] : ''; ?>"/>
 
         <label class="form__label" for="nbpages">Nombre de pages</label>
         <?= isset($data['errors']['nbpages']) ? '<span class="has-error">' . $data['errors']['nbpages'] . '</span>' : ''; ?></span>
@@ -50,14 +50,14 @@
                type="text"
                name="nbpages"
                id="nbpages"
-               value="<?= $data['d']['nbpages']; ?>"/>
+               value="<?= isset($data['d']['nbpages']) ? $data['d']['nbpages'] : ''; ?>"/>
 
         <label class="form__label" for="author_id">Selectionnez l'auteur</label>
         <select class="form__input"
                 name="author_id"
                 id="author_id">
             <?php foreach ($data['authors'] as $author): ?>
-                <option <?= ($data['d']['author_id'] == $author->id) ? 'selected="selected"' : '' ?>
+                <option <?= (isset($data['d']['author_id']) && $data['d']['author_id'] == $author->id) ? 'selected="selected"' : '' ?>
                     value="<?= $author->id; ?>"><?= $author->first_name . ' ' . $author->last_name; ?></option>
             <?php endforeach; ?>
         </select>
@@ -68,7 +68,7 @@
                 name="genre_id"
                 id="genre_id">
             <?php foreach ($data['genres'] as $genre): ?>
-                <option <?= ($data['d']['genre_id'] == $genre->id) ? 'selected="selected"' : '' ?>
+                <option <?= (isset($data['d']['genre_id']) && $data['d']['genre_id'] == $genre->id) ? 'selected="selected"' : '' ?>
                     value="<?= $genre->id; ?>"><?= $genre->name; ?></option>
             <?php endforeach; ?>
         </select>
@@ -78,7 +78,7 @@
                 name="language_id"
                 id="language_id">
             <?php foreach ($data['languages'] as $language): ?>
-                <option <?= ($data['d']['language_id'] == $language->id) ? 'selected="selected"' : ''; ?>
+                <option <?= (isset($data['d']['language_id']) && $data['d']['language_id'] == $language->id) ? 'selected="selected"' : ''; ?>
                     value="<?= $language->id; ?>"><?= $language->name; ?></option>
             <?php endforeach; ?>
         </select>
@@ -88,7 +88,7 @@
                 name="editor_id"
                 id="editor_id">
             <?php foreach ($data['editors'] as $editor): ?>
-                <option <?= ($data['d']['editor_id'] == $editor->id) ? 'selected="selected"' : '' ?>
+                <option <?= (isset($data['d']['editor_id']) && $data['d']['editor_id'] == $editor->id) ? 'selected="selected"' : '' ?>
                     value="<?= $editor->id; ?>"><?= $editor->name; ?></option>
             <?php endforeach; ?>
         </select>
@@ -99,7 +99,7 @@
                 name="location_id"
                 id="location_id">
             <?php foreach ($data['locations'] as $location): ?>
-                <option <?= ($data['d']['location_id'] == $location->id) ? 'selected="selected"' : '' ?>
+                <option <?= (isset($data['d']['location_id']) && $data['d']['location_id'] == $location->id) ? 'selected="selected"' : '' ?>
                     value="<?= $location->id; ?>"><?= $location->name; ?></option>
             <?php endforeach; ?>
         </select>
