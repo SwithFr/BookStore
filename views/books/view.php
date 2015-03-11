763620611bd5ffb045bafea2433d239224978009
Jeremy Smith <contact@swith.fr>
2015-03-11 18:51:54 +0100

    add view books/view

---
+++ views/books/view.php
@@ -0,0 +1,17 @@
+<div id="content" class="content">
+    <div class="section">
+        <div class="section__header">
+            <h2 class="section__title author__name">Titre du livre</h2>
+            <div class="section__block bio">
+                <p class="section__block__year">Publié en 2015</p>
+                <p class="section__block__category">Aventure</p>
+                <div class="section__block__content">
+                    <p>Dolor neque volutpat nec augue phasellus magna facilisis nam fusce cursus parturient maecenas. Ipsum metus nullam hendrerit ad aenean. Massa risus nam elit amet risus dapibus iaculis. Purus massa purus cum ultricies enim eni eget duis blandit. Lacus porta sapien vehicula odio dis. Justo lorem primis ornare eni viverra volutpat ad eros. Augue fusce. Dolor justo tincidunt eleifend nunc aliquam eu placerat. Dolor morbi nibh class ut habitant senectus mi massa ut arcu nibh praesent nunc taciti. Lacus porta a integer sociis nulla eu taciti neque dictum at sociis curae parturient diam metus. Fusce lorem</p>
+                    <p>ipsum semper eros. Ipsum class. Lacus class mattis in suscipit ultrices tortor. Dolor ipsum fames netus cum quam velit facilisis. Class metus. Fames neque massa netus nonummy luctus sem. Metus magna interdum orci est massa habitasse eros cubilia mi. Class lorem cras rhoncus mattis id ante erat id fames. Morbi metus nullam odio tempor in sed. Massa fusce nam laoreet. Morbi etiam a.</p>
+                    <p>velit odio nulla tempus justo egestas justo mi justo magnis elit. Magna etiam donec tempor vestibulum etiam cras pharetra. Class magna. Lacus class tortor feugiat magnis cras curabitur semper curabitur fusce. Morbi netus ultrices orci aliquet et tristique a class euismod conubia orci curabitur. Proin velit orci diam venenatis tellus nisl suspendisse eni orci. Dolor morbi euismod fermentum ultrices mi. Ipsum magna inceptos et suspendisse arcu ornare nullam laoreet in diam magna ad hendrerit iaculis. Etiam dolor hac. Massa metus turpis tellus hendrerit praesent. Magna ipsum. Lacus morbi.</p>
+                </div>
+            </div>
+            <div class="section__block infos"><img src="../../../assets/img/b1.png" class="section__block__img"></div>
+        </div>
+    </div>
+</div>
