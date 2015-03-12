a1bae9bf463b32ba763c5296350fbd82a32a4476
Jeremy Smith <contact@swith.fr>
2015-03-12 09:40:45 +0100

    update function index

--- controllers/GenresController.php
+++ controllers/GenresController.php
@@ -6,6 +6,15 @@ class GenresController extends AppController
 {
     public function index()
     {
-        
+        $this->loadModel('Book');
+        $books = $this->Book->getWithGenre();
+        $letters = [];
+        $i = 0;
+        foreach ($books as $book) {
+            $letters[$book->name][$i] = $book->title;
+            $i++;
+        }
+
+        return compact('letters');
     }
 } 
