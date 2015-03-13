0215cb44e65f5b8d2bec758fd6637cbd25282028
Jeremy Smith <contact@swith.fr>
2015-03-13 16:35:35 +0100

    update urls

--- views/elements/nav.php
+++ views/elements/nav.php
@@ -1,12 +1,13 @@
+<?php use Helpers\Html; ?>
 <nav id="nav" class="nav">
     <div class="nav__content">
         <h1 class="nav__title"><a id="main__title" href="<?= $_SERVER['PHP_SELF']; ?>">Book Store</a></h1>
         <ul class="nav__menu">
-            <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=author&letter=a">Auteurs</a></li>
-            <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=editor&letter=a">Editeurs</a></li>
-            <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=genre">Genres</a></li>
-            <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=popular&e=book">Classement</a></li>
-            <li class="nav__menu__item"><a href="<?= $_SERVER['PHP_SELF']; ?>?a=index&e=library">Bibliothèques</a></li>
+            <li class="nav__menu__item"><a href="<?= Html::url('index','author',['letter'=>'a']); ?>">Auteurs</a></li>
+            <li class="nav__menu__item"><a href="<?= Html::url('index','editor',['letter'=>'a']); ?>">Editeurs</a></li>
+            <li class="nav__menu__item"><a href="<?= Html::url('index','genre'); ?>">Genres</a></li>
+            <li class="nav__menu__item"><a href="<?= Html::url('populars','book'); ?>">Classement</a></li>
+            <li class="nav__menu__item"><a href="<?= Html::url('index','library'); ?>">Bibliothèques</a></li>
         </ul>
     </div>
 </nav>
