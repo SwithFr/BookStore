6597e2da777822de22643adbb655bdcd99c43313
Jeremy Smith <contact@swith.fr>
2015-03-13 16:42:19 +0100

    update view books/populars

---
+++ views/books/populars.php
@@ -0,0 +1,13 @@
+<?php require(D_VIEWS . DS . 'elements' . DS . 'main-form.php'); use Helpers\Html; ?>
+<div class="section">
+    <div class="section__header">
+        <h2 class="section__title">Les livres par genres</h2>
+    </div>
+
+    <?php foreach($data['books'] as $book): ?>
+        <h2><?= $book->title; ?></h2>
+        <p><?= $book->value; ?></p>
+
+    <?php endforeach; ?>
+
+</div>
