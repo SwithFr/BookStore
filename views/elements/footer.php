aa3ff3f345ab804318e8cf997b83d1eeec60ddcd
Jeremy Smith <contact@swith.fr>
2015-03-14 11:36:46 +0100

    update footer

--- views/elements/footer.php
+++ views/elements/footer.php
@@ -1,14 +1,15 @@
+<?php use Helpers\Html; ?>
 <footer class="footer">
     <div class="footer__content">
         <h2 class="footer__title">Book Store</h2>
         <nav class="nav">
             <div class="nav__content">
                 <ul class="nav__menu">
-                    <li class="nav__menu__item"><a href="./views/books/authorsList.html">Auteurs</a></li>
-                    <li class="nav__menu__item"><a href="./views/books/editorsList.html">Editeurs</a></li>
-                    <li class="nav__menu__item"><a href="./views/books/categoriesList.html">Genres</a></li>
-                    <li class="nav__menu__item"><a href="./views/books/languagesList.html">Langues</a></li>
-                    <li class="nav__menu__item"><a href="./views/books/yearsList.html">Années</a></li>
+                    <li class="nav__menu__item"><a href="<?= Html::url('index','author',['letter'=>'a']); ?>">Auteurs</a></li>
+                    <li class="nav__menu__item"><a href="<?= Html::url('index','editor',['letter'=>'a']); ?>">Editeurs</a></li>
+                    <li class="nav__menu__item"><a href="<?= Html::url('index','genre'); ?>">Genres</a></li>
+                    <li class="nav__menu__item"><a href="<?= Html::url('populars','book'); ?>">Classement</a></li>
+                    <li class="nav__menu__item"><a href="<?= Html::url('index','library'); ?>">Bibliothèques</a></li>
                 </ul>
             </div>
         </nav>
