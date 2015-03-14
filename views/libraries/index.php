42a28a55d1d08435c18ba3a50386cdf6d37c9c3b
Jeremy Smith <contact@swith.fr>
2015-03-14 11:44:50 +0100

    add view libraries/index

---
+++ views/libraries/index.php
@@ -0,0 +1,14 @@
+<?php require(D_VIEWS . DS . 'elements' . DS . 'main-form.php'); use Helpers\Html; ?>
+<div class="section">
+    <div class="section__header">
+        <h2 class="section__title">Les bibliothèques</h2>
+    </div>
+    <ul>
+        <?php foreach($data['libraries'] as $library): ?>
+            <li>
+                <a href="<?= Html::url('view','librarie',['id'=>$library->id]); ?>"><?= $library->name; ?></a>
+            </li>
+        <?php endforeach; ?>
+    </ul>
+</div>
+</div>
