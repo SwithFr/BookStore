f3e8d7477d43e750d458b7a20e53551b4c546d9b
Jeremy Smith <contact@swith.fr>
2015-03-17 13:20:20 +0100

    update view editors/index

--- views/editors/index.php
+++ views/editors/index.php
@@ -12,19 +12,17 @@
         <?php endfor; ?>
 
     </ul>
-    <div class="section__block">
-        <?php if(empty($data['editors'])): ?>
-            <h2>Aucun éditeur trouvé !</h2>
-        <?php endif; ?>
-        <ul class="editors__list">
-            <?php foreach($data['editors'] as $editor): ?>
-                <li class="editors__list__item">
-                    <img src="<?= D_ASSETS . DS . 'img' . DS . $editor->img; ?>" class="editor__img">
-                    <h2 class="editor__title"><a href="./editorSingle.html"><?= $editor->name; ?></a></h2>
-                    <span class="editor__infos">30 livres</span>
-                    <p><?= $editor->history; ?></p>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-    </div>
+    <?php if(empty($data['editors'])): ?>
+        <h2 class="noResult">Aucun éditeur trouvé !</h2>
+    <?php endif; ?>
+    <ul class="editors__list">
+        <?php foreach($data['editors'] as $editor): ?>
+            <li class="section__block  editors__list__item">
+                <img src="<?= D_ASSETS . DS . 'img' . DS . $editor->img; ?>" class="editor__img">
+                <h2 class="editor__title"><a href="./editorSingle.html"><?= $editor->name; ?></a></h2>
+                <span class="editor__infos">30 livres</span>
+                <p><?= $editor->history; ?></p>
+            </li>
+        <?php endforeach; ?>
+    </ul>
 </div>
