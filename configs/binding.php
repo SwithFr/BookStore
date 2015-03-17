9a62eb7751d8a561eeaff985a400bad65419ea5a
Jeremy Smith <contact@swith.fr>
2015-03-17 10:24:53 +0100

    add binding to Author

--- configs/binding.php
+++ configs/binding.php
@@ -1,4 +1,5 @@
 <?php
  return [
-    Models\Interfaces\BooksRepositoryInterface::class => Models\Book::class
+    Models\Interfaces\BooksRepositoryInterface::class => Models\Book::class,
+    Models\Interfaces\AuthorsRepositoryInterface::class => Models\Author::class
  ];
