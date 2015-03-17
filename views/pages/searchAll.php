41d7ad5490272382a24274518ef3f019d11959e2
Jeremy Smith <contact@swith.fr>
2015-03-17 12:07:53 +0100

    update view pages/searchAll

--- views/pages/searchAll.php
+++ views/pages/searchAll.php
@@ -1,28 +1,32 @@
 <?php require(D_VIEWS . DS . 'elements' . DS . 'main-form.php');
 use Helpers\Html;
+
 ?>
 <div class="section">
-    <?php if(!isset($data['request'])): ?>
+    <?php if (!isset($data['request'])): ?>
         <div class="section__header">
             <h2 class="section__title">Aucune recherche formulée !</h2>
         </div>
     <?php else: ?>
         <div class="section__header">
-            <h2 class="section__title">Résultats de la recherche pour <?= $data['request']; ?></h2>
+            <h2 class="section__title">Résultats de la recherche pour <span
+                    class="request"><?= $data['request']; ?></span></h2>
         </div>
         <?php if (empty($data['data']['libraries'])): ?>
             <h2 class="noResult">Aucun livre trouvé !</h2>
         <?php else: ?>
             <div class="section__block results">
                 <h2 class="results__title">Le(s) Livre(s)</h2>
-                <?php foreach($data['data']['libraries'] as $k => $v): ?>
-                    <p>
-                        Dans la bibliothèque : <?= $k; ?>
+                <?php foreach ($data['data']['libraries'] as $k => $v): ?>
+                    <p class="library">
+                        Dans la bibliothèque : <a class="library__name" href="<?= Html::url('view', 'librarie', ['id' => $v['id']]); ?>"><?= $k; ?></a>
                     </p>
                     <?php foreach ($v as $book): ?>
-                        <h3 class="results__link">
-                            <a href="<?= Html::url('view', 'book', ['id' => $book->id]); ?>"><?= $book->title; ?></a>
-                        </h3>
+                        <?php if (!is_numeric($book)): ?>
+                            <h3>
+                                <a class="results__link" href="<?= Html::url('view', 'book', ['id' => $book->id]); ?>"><?= $book->title; ?></a>
+                            </h3>
+                        <?php endif; ?>
                     <?php endforeach; ?>
                 <?php endforeach; ?>
             </div>
