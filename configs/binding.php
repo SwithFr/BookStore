d164e92aa810fbcc34a260daa0e6b77644784caf
Jeremy Smith <contact@swith.fr>
2015-03-17 10:57:43 +0100

    add UsersInterfaces

--- configs/binding.php
+++ configs/binding.php
@@ -3,5 +3,6 @@
     Models\Interfaces\BooksRepositoryInterface::class => Models\Book::class,
     Models\Interfaces\AuthorsRepositoryInterface::class => Models\Author::class,
     Models\Interfaces\EditorsRepositoryInterface::class => Models\Editor::class,
-     Models\Interfaces\GenresRepositoryInterface::class => Models\Genre::class
+    Models\Interfaces\GenresRepositoryInterface::class => Models\Genre::class,
+    Models\Interfaces\UsersRepositoryInterface::class => Models\User::class
  ];
