89b22aea23a89dbe211a7ffd53ddb1be60c9b83f
Jeremy Smith <contact@swith.fr>
2015-03-17 13:57:24 +0100

    update view genres/index

--- views/genres/index.php
+++ views/genres/index.php
@@ -1,14 +1,21 @@
-<?php require(D_VIEWS . DS . 'elements' . DS . 'main-form.php'); use Helpers\Text; use Helpers\Html; ?>
+<?php require(D_VIEWS . DS . 'elements' . DS . 'main-form.php');
+use Helpers\Html;
+
+?>
 <div class="section">
     <div class="section__header">
         <h2 class="section__title">Les livres par genres</h2>
     </div>
 
-    <?php foreach($data['letters'] as $k => $v): ?>
-        <h1><?= $k; ?></h1>
-        <?php foreach($v as $w): ?>
-            <p><?= $w; ?></p>
-        <?php endforeach; ?>
+    <?php foreach ($data['letters'] as $k => $v): ?>
+        <div class="genre">
+            <h2 class="section__block__title"><?= $k; ?></h2>
+            <?php foreach ($v as $w): ?>
+                <p>
+                    <a class="results__link" href="<?= Html::url('view', 'genre', ['id' => $w->id]); ?>"><?= $w->title; ?></a>
+                </p>
+            <?php endforeach; ?>
+        </div>
     <?php endforeach; ?>
 
 </div>
