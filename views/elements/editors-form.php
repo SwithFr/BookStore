1686a92a925f873a58adf548387b7e99c0f3838e
Jeremy Smith <contact@swith.fr>
2015-04-14 18:49:24 +0200

    update form editors-form search

--- views/elements/editors-form.php
+++ views/elements/editors-form.php
@@ -1,4 +1,7 @@
-<form class="form">
-    <input placeholder="Chercher un editeur..." class="form__input form--search">
+<form action="<?= \Helpers\Html::url('search','editor'); ?>" class="form" method="post">
+    <input placeholder="Chercher un editeur..."
+           class="form__input form--search"
+           name="search"
+           id="search">
     <input type="submit" class="form__submit form--search--submit">
 </form>
