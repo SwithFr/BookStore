69448e7eafee1ff567580cd0a8345776c505ff1a
Jeremy Smith <contact@swith.fr>
2015-04-17 18:42:37 +0200

    fix text

--- views/locations/add.php
+++ views/locations/add.php
@@ -3,7 +3,7 @@
     <h2 class="section__title">Ajouter un emplacement à la bibliothèque</h2>
 
     <form class="form--add" action="<?= Html::url('add', 'location'); ?>&library=<?= $data['library_id']; ?>" method="post">
-        <label class="form__label" for="name">Nom de l'éditeur</label>
+        <label class="form__label" for="name">Nom de l'emplacement</label>
         <?= isset($data['errors']['name']) ? '<span class="has-error">' . $data['errors']['name'] . '</span>' : ''; ?></span>
         <input class="form__input form__input--large"
                type="text"
