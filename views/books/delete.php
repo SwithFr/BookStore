bbccf84043f62895b802b67c33b9e448c87fb448
Jeremy Smith <contact@swith.fr>
2015-05-01 12:10:04 +0200

    add delete view

---
+++ views/books/delete.php
@@ -0,0 +1,18 @@
+<?php use Helpers\Html; ?>
+<?php use Helpers\Text; ?>
+<div class="section">
+    <div class="section__block">
+        <img src="<?= $data['book']->img; ?>" class="section__block__img">
+
+        <h3 class="section__block__title"><a
+                href="<?= Html::url('view', 'book', ['id' => $data['book']->id]); ?>"><?= $data['book']->title; ?></a></h3>
+
+        <p class="section__block__author"><?= $data['book']->first_name . ' ' . $data['book']->last_name; ?></p>
+
+        <p class="section__block__content">
+            <?= Text::cut($data['book']->summary, 500); ?>
+        </p>
+    </div>
+</div>
+<a class="btn btn--delete btn--inline" href="<?= Html::url('goDelete','book',['id'=>$data['book']->id]); ?>">Supprimer</a>
+<a class="btn btn--add btn--inline" href="<?= Html::url('account','user'); ?>">Annuler</a>
