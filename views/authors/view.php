e3dccdb97775cac2239b2896332ba385f57ef866
Jeremy Smith <contact@swith.fr>
2015-05-02 16:12:23 +0200

    add links to vote

--- views/authors/view.php
+++ views/authors/view.php
@@ -1,27 +1,37 @@
-<?php use Helpers\Html; ?>
-<?php if($data['author']): ?>
+<?php
+use Helpers\Html;
+
+$v = new \Behaviors\Votable();
+?>
+<?php if ($data['author']): ?>
     <div id="content" class="content">
         <div class="section">
             <div class="section__header">
                 <h2 class="section__title"><?= $data['author']->first_name . ' ' . $data['author']->last_name; ?></h2>
+
                 <div class="section__block bio">
                     <p class="section__block__year"><?= $data['author']->date_birth; ?><?= ($data['author']->date_death !== '') ? ' - ' . $data['author']->date_death : ''; ?></p>
+
                     <div class="section__block__content">
                         <?= $data['author']->bio; ?>
                     </div>
                 </div>
                 <div class="section__block infos">
                     <img src="<?= $data['author']->img; ?>" class="section__block__img">
-                    <?php if(!empty($data['books'])): ?>
+                    <?php if (!empty($data['books'])): ?>
                         <h3>Voir ses livres</h3>
                         <ul id="books__list">
-                            <?php foreach($data['books'] as $book): ?>
+                            <?php foreach ($data['books'] as $book): ?>
                                 <li>
-                                    <a href="<?= Html::url('view','book',['id'=>$book->id]); ?>"><?= $book->title; ?></a>
+                                    <a href="<?= Html::url('view', 'book', ['id' => $book->id]); ?>"><?= $book->title; ?></a>
                                 </li>
                             <?php endforeach; ?>
                         </ul>
                     <?php endif; ?>
+                    <div class="<?= $v->getClass('authors', $data['author']->id); ?>">
+                        <a class="voteUp" href="<?= Html::url('voteUp', 'author', ['ref' => 'authors', 'ref_id' => $data['author']->id]); ?>">J'aime cet auteur</a> /
+                        <a class="voteDown" href="<?= Html::url('voteDown', 'author', ['ref' => 'authors', 'ref_id' => $data['author']->id]); ?>">Je n'aime pas cet auteur</a>
+                    </div>
                 </div>
             </div>
         </div>
