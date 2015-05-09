4c3cd8eca59627f67f1e30f85bf13713d863cf15
Jeremy Smith <contact@swith.fr>
2015-05-09 19:41:21 +0200

    html

--- views/books/view.php
+++ views/books/view.php
@@ -1,4 +1,4 @@
-<?php use Helpers\Html; ?>
+<?php use Helpers\Html; use Components\Session; ?>
 <?php $v = new \Behaviors\Votable(); ?>
 <?php if($data['book']): ?>
     <div class="section">
@@ -16,17 +16,20 @@
             </div>
             <div class="section__block infos">
                 <img src="<?= $data['book']->img; ?>" class="section__block__img">
-                <p>Numéro ISBN : <?= $data['book']->isbn; ?></p>
-                <p>Nombre de pages : <?= $data['book']->nbpages; ?></p>
-            </div>
-            <?php if( \Components\Session::isLogged() ): ?>
-                <div class="<?= $v->getClass('books', $data['book']->id); ?>">
-                    <a class="voteUp" href="<?= Html::url('voteUp', 'book', ['ref_id' => $data['book']->id]); ?>">J'aime ce livre</a> /
-                    <a class="voteDown" href="<?= Html::url('voteDown', 'book', ['ref_id' => $data['book']->id]); ?>">Je n'aime pas ce livre</a>
+                <p><span class="info__name">Numéro ISBN : </span><?= $data['book']->isbn; ?></p>
+                <p><span class="info__name">Nombre de pages : </span><?= $data['book']->nbpages; ?></p>
+                <div class="votes">
+                    <?php if(Session::isLogged()): ?>
+                        <div class="<?= $v->getClass('books', $data['book']->id); ?>">
+                            <a class="voteUp" href="<?= Html::url('voteUp', 'book', ['ref_id' => $data['book']->id]); ?>">J'aime ce livre</a> /
+                            <a class="voteDown" href="<?= Html::url('voteDown', 'book', ['ref_id' => $data['book']->id]); ?>">Je n'aime pas ce livre</a>
+                        </div>
+                    <?php else: ?>
+                        <p class="section__block__infos">Connectez-vous ou créez un compte pour voter pour ce livre.</p>
+                    <?php endif; ?>
+                    <p>Note globale de <span class="nb"><?= $data['book']->vote; ?></span> sur 5</p>
                 </div>
-            <?php endif; ?>
-            <p>Note globale de <span class="nb"><?= $data['book']->vote; ?></span> sur 5</p>
-            <p class="section__block__infos">Connectez-vous ou créez un compte pour voter pour ce livre.</p>
+            </div>
         </div>
     </div>
 <?php endif; ?>
