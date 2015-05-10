caa1d6e9643f9d59153bbdcf610875cef8953eae
Jeremy Smith <contact@swith.fr>
2015-05-10 21:48:40 +0200

    clean

--- views/authors/view.php
+++ views/authors/view.php
@@ -6,23 +6,23 @@ $v = new \Behaviors\Votable();
 <?php if ($data['author']): ?>
     <div class="section">
         <div class="section__header">
-            <h2 class="section__title"><?= $data['author']->first_name . ' ' . $data['author']->last_name; ?></h2>
+            <h2 class="section__title"><?= $data['author']->name(); ?></h2>
 
             <div class="section__block bio">
-                <p class="section__block__year"><?= $data['author']->date_birth; ?><?= ($data['author']->date_death !== '') ? ' - ' . $data['author']->date_death : ''; ?></p>
+                <p class="section__block__year"><?= $data['author']->date(); ?></p>
 
                 <div class="section__block__content">
                     <?= $data['author']->bio; ?>
                 </div>
             </div>
             <div class="section__block infos">
-                <img src="<?= $data['author']->img; ?>" class="section__block__img">
+                <?= $data['author']->img(); ?>
                 <?php if (!empty($data['books'])): ?>
                     <h3>Voir ses livres</h3>
                     <ul id="books__list">
                         <?php foreach ($data['books'] as $book): ?>
                             <li>
-                                <a href="<?= Html::url('view', 'book', ['id' => $book->id]); ?>"><?= $book->title; ?></a>
+                                <a href="<?= $book->link(); ?>"><?= $book->title; ?></a>
                             </li>
                         <?php endforeach; ?>
                     </ul>
