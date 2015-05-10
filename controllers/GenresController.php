79a5ebfcfcbef121ddf6a688bc07df9a563d181b
Jeremy Smith <contact@swith.fr>
2015-05-10 16:45:13 +0200

    add function view

--- controllers/GenresController.php
+++ controllers/GenresController.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use Components\Request;
+use Components\Session;
 use Models\Interfaces\GenresRepositoryInterface;
 
 class GenresController extends AppController
@@ -20,14 +21,29 @@ class GenresController extends AppController
     public function index()
     {
         $this->loadModel('Book');
-        $books = $this->Book->getWithGenre();
-        $letters = [];
-        $i = 0;
-        foreach ($books as $book) {
-            $letters[$book->name][$i] = $book;
-            $i++;
+        $genres = $this->Genre->get();
+        foreach ($genres as $genre) {
+            $genre->books = $this->Book->getWithGenre($genre->id, 5);
+            $genre->book_count = $this->Book->count('genre_id = ' . $genre->id);
         }
 
-        return compact('letters');
+        return compact('genres');
+    }
+
+    public function viex()
+    {
+        if (!$this->request->id) {
+            $this->redirect('missingParams', 'error');
+        }
+
+        $genre = $this->Genre->find($_GET['id']);
+        if(!$genre) {
+            Session::setFlash('Cette catégorie n‘existe pas','error');
+            $this->redirect('index','genre');
+        }
+
+        $genre->books =  $genre->books = $this->Book->getWithGenre($genre->id);
+
+        return compact('genre');
     }
 } 
