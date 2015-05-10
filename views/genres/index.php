b60f8837ed3962a9a3a3732676bd9c722f7c885c
Jeremy Smith <contact@swith.fr>
2015-05-10 17:05:30 +0200

    update view

--- views/genres/index.php
+++ views/genres/index.php
@@ -8,14 +8,22 @@ use Helpers\Html;
     </div>
     <?php foreach ($data['genres'] as $genre): ?>
         <div class="genre">
-            <h2 class="section__block__title title--linked"><a href="<?= Html::url('view','genre',['id'=>$genre->id]); ?>"><?= $genre->name; ?></a> (<?= $genre->book_count; ?>)</h2>
+            <h2 class="section__block__title title--linked">
+                <a href="<?= Html::url('view','genre',['id'=>$genre->id]); ?>"><?= $genre->name; ?></a>
+                <?php if($genre->book_count > 0): ?>
+                    (<?= $genre->book_count; ?>)
+                <?php endif; ?>
+
+            </h2>
             <?php if(!empty($genre->books)): ?>
                 <?php foreach ($genre->books as $book): ?>
                     <p>
                         <a class="results__link" href="<?= Html::url('view', 'book', ['id' => $book->id]); ?>"><?= $book->title; ?></a>
                     </p>
                 <?php endforeach; ?>
-                <p><a class="results__link" href="<?= Html::url('view','genre',['id'=>$genre->id]); ?>">&hellip;</a></p>
+                <?php if($genre->book_count > count($genre->books)): ?>
+                    <p><a class="results__link" href="<?= Html::url('view','genre',['id'=>$genre->id]); ?>">&hellip;</a></p>
+                <?php endif; ?>
             <?php else: ?>
                 <p class="empty">Pas de livre dans cette catégorie</p>
             <?php endif; ?>
