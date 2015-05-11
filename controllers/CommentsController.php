13f64cc95a169ff949d242aeeb0e7e1734295dc8
Jeremy Smith <contact@swith.fr>
2015-05-11 17:22:05 +0200

    update comments controller

--- controllers/CommentsController.php
+++ controllers/CommentsController.php
@@ -4,6 +4,9 @@
 namespace Controllers;
 
 
+use Components\Request;
+use Components\Session;
+use Components\Validator;
 use Models\Interfaces\CommentsRepositoryInterface;
 
 class CommentsController extends AppController
@@ -14,14 +17,36 @@ class CommentsController extends AppController
         $this->Comment = $comment;
     }
 
-    public function add($ref, $ref_id)
+    public function add()
     {
+        if(!$this->request->isPost()) {
+            $this->redirect('wrangMethod','error');
+        }
 
+        if(!$this->request->checkParams(['ref'=>'string','ref_id'=>'id'])){
+            $this->redirect('missingParams','error');
+        }
+
+        $v = new Validator();
+        if($v->validate($_POST,$this->Comment->rules)) {
+            $this->Comment->create($_POST['text'], $_GET['ref'], $_GET['ref_id']);
+            Session::setFlash('Votre commentaire à bien été ajouté');
+        } else {
+            Session::setFlash('Veuillez verifier vos entrées','error');
+            $v->sendError(['text']);
+        }
+        $this->redirect('view',$_GET['ref'],['id'=>$_GET['ref_id']]);
     }
 
-    public function delete($ref, $ref_id)
+    public function delete()
     {
+        if(!$this->request->checkParams(['ref'=>'string','ref_id'=>'id'])){
+            $this->redirect('missingParams','error');
+        }
 
+        $this->Comment->delete($_GET['ref_id'],'ref = ' . $_GET['ref'] . ' AND ref_id = ' . $_GET['ref_id'] );
+        $this->Comment->updateUserCount(Session::getId());
+        Session::setFlash('Votre commentaire a bien été supprimé !');
     }
 
 } 
