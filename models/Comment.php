65f17e4c853ab5dfece0a780db6ae8ff9a6096b6
Jeremy Smith <contact@swith.fr>
2015-05-11 15:54:14 +0200

    add create function

--- models/Comment.php
+++ models/Comment.php
@@ -4,9 +4,36 @@
 namespace Models;
 
 
+use Components\Session;
 use Models\Interfaces\CommentsRepositoryInterface;
 
 class Comment extends AppModel implements CommentsRepositoryInterface
 {
+    public $rules = [
+        'text' => [
+            ['ruleName'=>'notEmpty','message'=>'Veuillez saisir un commentaire']
+        ]
+    ];
 
+    public function create($comment, $ref, $ref_id)
+    {
+        $sql = 'INSERT INTO comments (comment, ref, ref_id, user_id) VALUES (:comment, :ref, :ref_id, :user_id)';
+        $pdost = $this->db->prepare($sql);
+        $pdost->execute([
+            ':comment' => $comment,
+            ':ref' => $ref,
+            ':ref_id' => $ref_id,
+            ':user_id' => Session::getId()
+        ]);
+
+        $sql = 'UPDATE :ref SET comment_count = comment_count + 1 WHERE id = :ref_id';
+        $pdost = $this->db->prepare($sql);
+        $pdost->execute([
+            ':ref' => 'books',
+            ':ref_id' => $ref_id
+        ]);
+
+        $sql = 'UPDATE users SET comment_count = comment_count + 1 WHERE id = ' . Session::getId();
+        $this->db->query($sql);
+    }
 } 
