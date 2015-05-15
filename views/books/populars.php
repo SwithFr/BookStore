a03233ddb26c7e177e238848eeb2ebc4bce4ef93
Jeremy Smith <contact@swith.fr>
2015-05-15 17:49:22 +0200

    add populars authors

--- views/books/populars.php
+++ views/books/populars.php
@@ -1,13 +1,21 @@
 <?php require(D_VIEWS . DS . 'elements' . DS . 'main-form.php'); use Helpers\Html; ?>
 <div class="section">
-    <div class="section__header">
-        <h2 class="section__title">Classement</h2>
+    <div class="section__block">
+        <div class="section__header">
+            <h2 class="section__block__title">Classement des 10 meilleurs livres</h2>
+        </div>
+        <?php foreach($data['books'] as $book): ?>
+            <h2><?= $book->title; ?></h2>
+            <p><?= $book->score(); ?></p>
+        <?php endforeach; ?>
     </div>
-
-    <?php foreach($data['books'] as $book): ?>
-        <h2><?= $book->title; ?></h2>
-        <p><?= $book->vote; ?></p>
-
-    <?php endforeach; ?>
-
+    <div class="section__block populars__authors">
+        <div class="section__header">
+            <h2 class="section__block__title">Classement des 10 meilleurs Auteurs</h2>
+        </div>
+        <?php foreach($data['authors'] as $author): ?>
+            <h2><?= $author->name(); ?></h2>
+            <p><?= $author->score(); ?></p>
+        <?php endforeach; ?>
+    </div>
 </div>
