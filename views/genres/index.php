c0c3fea9bc7b6c7f72b7128fc886ff10ad2d7378
Jeremy Smith <contact@swith.fr>
2015-05-15 17:22:32 +0200

    remove count

--- views/genres/index.php
+++ views/genres/index.php
@@ -10,10 +10,6 @@ use Helpers\Html;
         <div class="genre">
             <h2 class="section__block__title title--linked">
                 <a href="<?= Html::url('view','genre',['id'=>$genre->id]); ?>"><?= $genre->name; ?></a>
-                <?php if($genre->book_count > 0): ?>
-                    (<?= $genre->book_count; ?>)
-                <?php endif; ?>
-
             </h2>
             <?php if(!empty($genre->books)): ?>
                 <?php foreach ($genre->books as $book): ?>
@@ -21,9 +17,7 @@ use Helpers\Html;
                         <a class="results__link" href="<?= Html::url('view', 'book', ['id' => $book->id]); ?>"><?= $book->title; ?></a>
                     </p>
                 <?php endforeach; ?>
-                <?php if($genre->book_count > count($genre->books)): ?>
-                    <p><a class="results__link" href="<?= Html::url('view','genre',['id'=>$genre->id]); ?>">&hellip;</a></p>
-                <?php endif; ?>
+                <p><a class="results__link" href="<?= Html::url('view','genre',['id'=>$genre->id]); ?>">&hellip;</a></p>
             <?php else: ?>
                 <p class="empty">Pas de livre dans cette catégorie</p>
             <?php endif; ?>
