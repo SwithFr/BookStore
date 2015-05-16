1d3232748d22098e8dec82fcfd251f4b43686640
Jeremy Smith <contact@swith.fr>
2015-05-16 11:34:13 +0200

    add delete author view

---
+++ views/authors/delete.php
@@ -0,0 +1,16 @@
+<?php use Helpers\Html; ?>
+<?php use Helpers\Text; ?>
+<div class="section">
+    <div class="section__block">
+        <?= $data['author']->img(); ?>
+
+        <h3 class="section__block__title"><a
+                href="<?= $data['author']->link(); ?>"><?= $data['author']->name(); ?></a></h3>
+
+        <p class="section__block__content">
+            <?= $data['author']->sumUp(250); ?>
+        </p>
+    </div>
+</div>
+<a class="btn btn--delete btn--inline" href="<?= Html::url('goDelete','author',['id'=>$data['author']->id]); ?>">Supprimer</a>
+<a class="btn btn--add btn--inline" href="<?= Html::url('index','user'); ?>">Annuler</a>
