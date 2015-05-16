73336f60a65fa7ecd48c558511fb0f69323b934a
Jeremy Smith <contact@swith.fr>
2015-05-16 14:12:31 +0200

    fix redirect and find

--- controllers/EditorsController.php
+++ controllers/EditorsController.php
@@ -57,9 +57,9 @@ class EditorsController extends AppController
                 $dest = $name = '';
             }
 
-            $this->Editor->create($_POST['name'], $_POST['website'], $dest . $name, $_POST['history']);
+            $this->Editor->create($_POST['name'], $_POST['website'], $dest . $name, $_POST['history'],Session::getId());
             Session::setFlash('L‘éditeur ' . $_POST['first_name'] . ' ' . $_POST['last_name'] . ' a bien été ajouté !');
-            $this->redirect('edit', 'book');
+            $this->redirect('index', 'user');
         }
     }
 
@@ -89,7 +89,7 @@ class EditorsController extends AppController
             $this->redirect('missingParams', 'error');
         }
 
-        $editor = $this->Editor->find($_GET['id']);
+        $editor = $this->Editor->find(null,$_GET['id']);
         if (!$editor) {
             Session::setFlash('L‘éditeur est introuvable !', 'error');
         }
