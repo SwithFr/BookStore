158ea8f37ecea96f98c895cdcf3655ec5aeb8712
Jeremy Smith <contact@swith.fr>
2015-05-16 16:18:01 +0200

    add back link

--- views/authors/manage.php
+++ views/authors/manage.php
@@ -1,11 +1,12 @@
 <?php use Helpers\Html; ?>
-
+<p class="ariane"><a href="<?= Html::url('index','user'); ?>">Retour</a></p>
 <?php if(!$data['authors']): ?>
     <p class="alert alert--warning clearfix">
         Vous n'avez pas encore ajouté d'auteur
     </p>
     <a class="btn btn--add btn--inline" href="<?= Html::url('add','author'); ?>">Ajouter un auteur</a>
 <?php else: ?>
+    <a class="btn btn--add btn--inline" href="<?= Html::url('add','author'); ?>">Ajouter un auteur</a>
     <div class="section__block section__block--library clearfix">
         <h3 class="section__block__title--library">Auteurs que vous avez ajoutés</h3>
         <table class="books__list">
