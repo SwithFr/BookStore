18018d9cee0cadf3a62f3bfdaf771b2d783b56f7
Jeremy Smith <contact@swith.fr>
2015-05-17 15:39:41 +0200

    update form action

--- views/locations/add.php
+++ views/locations/add.php
@@ -2,7 +2,7 @@
 <div class="section">
     <h2 class="section__title">Ajouter un emplacement à la bibliothèque</h2>
 
-    <form class="form--add" action="<?= Html::url('add', 'location'); ?>&library=<?= $data['library_id']; ?>"
+    <form class="form--add" action="<?= Html::url('add', 'location', ['library' => $_GET['library']]); ?>"
           method="post">
         <label class="form__label" for="name">Nom de l'emplacement</label>
         <?= isset($data['errors']['name']) ? '<span class="has-error">' . $data['errors']['name'] . '</span>' : ''; ?></span>
