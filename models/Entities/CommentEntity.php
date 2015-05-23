bfb29a32c4202c5f01ab9d5846213470a46d17ab
Jeremy Smith <contact@swith.fr>
2015-05-23 17:37:26 +0200

    add comment

--- models/Entities/CommentEntity.php
+++ models/Entities/CommentEntity.php
@@ -8,6 +8,10 @@ use Helpers\Html;
 
 class CommentEntity
 {
+    /**
+     * Génère le lien de suppression d'un commentaire
+     * @return string
+     */
     public function deleteLink()
     {
         return '<a href="' . Html::url('delete', 'comment', ['ref' => $this->ref, 'ref_id' => $this->ref_id, 'comment_id' => $this->id]) . '" class="com--delete">Supprimer</a>';
