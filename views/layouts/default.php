107a23fcd11d7cb0dd57b0061d5ef9dd0db7b5fa
Jeremy Smith <contact@swith.fr>
2015-05-26 18:53:00 +0200

    syntaxe

--- views/layouts/default.php
+++ views/layouts/default.php
@@ -2,9 +2,8 @@
 
 <?php require(D_VIEWS . DS . 'elements' . DS . 'nav.php'); ?>
 
-<div id="content" class="content">
-    <?php \Components\Session::flash(); ?>
-    <?php include(D_VIEWS . DS . $controller->view); ?>
-</div>
-
+    <div id="content" class="content">
+        <?php \Components\Session::flash(); ?>
+        <?php include(D_VIEWS . DS . $controller->view); ?>
+    </div>
 <?php require(D_VIEWS . DS . 'elements' . DS . 'footer.php'); ?>
