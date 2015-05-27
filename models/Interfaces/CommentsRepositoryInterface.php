fffe75517acfc079cd6b5ce3de10b8828806a303
Jeremy Smith <contact@swith.fr>
2015-05-27 14:08:30 +0200

    update interfaces

--- models/Interfaces/CommentsRepositoryInterface.php
+++ models/Interfaces/CommentsRepositoryInterface.php
@@ -6,5 +6,27 @@ namespace Models\Interfaces;
 
 interface CommentsRepositoryInterface
 {
+    /**
+     * Ajoute un commentaire
+     * @param $comment
+     * @param $ref
+     * @param $ref_id
+     */
+    public function create($comment, $ref, $ref_id);
 
+    /**
+     * Pagine les commentaires pour un livre
+     * @param $nbpages
+     * @param $nbperpage
+     * @param $book_id
+     * @param $ref
+     * @return array
+     */
+    public function paginate($nbpages, $nbperpage, $book_id, $ref);
+
+    /**
+     * Réduit le nombre de commentaire d'un utilisateur
+     * @param $user_id
+     */
+    public function updateUserCount($user_id);
 } 
