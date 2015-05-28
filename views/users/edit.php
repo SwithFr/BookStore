f90075b71794afa13568c109809da5c5e81090d9
Jeremy Smith <contact@swith.fr>
2015-05-28 17:53:26 +0200

    update edit users view

--- views/users/edit.php
+++ views/users/edit.php
@@ -6,17 +6,25 @@
 
     <form class="form--add" action="<?= Html::url('edit', 'user'); ?>" method="post">
         <label class="form__label" for="login">Votre identifiant :</label>
-        <input class="form__input form__input--large" type="text" name="login" id="login"
+        <?= isset($data['errors']['login']) ? '<span class="has-error">' . $data['errors']['login'] . '</span>' : ''; ?></span>
+        <input class="form__input form__input--small" type="text" name="login" id="login"
                value="<?= $data['user']->login; ?>"/>
+
         <label class="form__label" for="email">Votre email :</label>
-        <input class="form__input form__input--large" type="text" name="email" id="email"
+        <?= isset($data['errors']['email']) ? '<span class="has-error">' . $data['errors']['email'] . '</span>' : ''; ?></span>
+        <input class="form__input form__input--small" type="text" name="email" id="email"
                value="<?= $data['user']->email; ?>"/>
-        <label class="form__label" for="password">Changez votre mot de passe <span class="section__block__infos">(laissez vide si vous ne voulez pas changer)</span> :</label>
-        <input class="form__input form__input--large" type="password" name="password" id="password"
-               value="fakePasswordForLol"/>
-        <label class="form__label" for="login">Confirmez le changement :</label>
-        <input class="form__input form__input--large" type="text" name="login" id="login"
+
+        <label class="form__label" for="chgPassword">Changez votre mot de passe <span class="section__block__infos">(laissez vide si vous ne voulez pas changer)</span> :</label>
+        <?= isset($data['errors']['passwords']) ? '<span class="has-error">' . $data['errors']['passwords'] . '</span>' : ''; ?></span>
+        <input class="form__input form__input--small" type="password" name="chgPassword" id="chgPassword"
                />
+
+        <label class="form__label" for="confirm">Confirmez le changement :</label>
+        <?= isset($data['errors']['passwords']) ? '<span class="has-error">' . $data['errors']['passwords'] . '</span>' : ''; ?></span>
+        <input class="form__input form__input--small" type="password" name="confirm" id="confirm"
+               />
+
         <input class="form__submit btn btn--send"
                type="submit"
                value="Modifier mes informations"/>
