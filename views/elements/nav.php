e5ed6ef5e99591fc589c20c8937e866f97c5f288
Jeremy Smith <contact@swith.fr>
2015-06-07 21:36:29 +0200

    update link

--- views/elements/nav.php
+++ views/elements/nav.php
@@ -1,7 +1,7 @@
 <?php use Helpers\Html; ?>
 <nav id="nav" class="nav">
     <div class="nav__content">
-        <h1 class="nav__title"><a id="main__title" href="<?= $_SERVER['PHP_SELF']; ?>">Book Store</a></h1>
+        <h1 class="nav__title"><a id="main__title" href="<?= Html::url('index', 'book'); ?>">Book Store</a></h1>
         <ul class="nav__menu">
             <li class="nav__menu__item"><a href="<?= Html::url('index', 'author', ['letter' => 'a']); ?>">Auteurs</a>
             </li>
